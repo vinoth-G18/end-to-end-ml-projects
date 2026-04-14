bd0fa3fc18e77d25d66cc3822a1bad83dfa6438d
vinoth-G18 <vinothvinothg1812@gmail.com>
2026-04-14 11:54:41 +0530

    data ingestion and transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -13,7 +13,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocesser_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
+    preprocesser_obj_file_path = os.path.join('artifact','preprocessor.pkl')
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config= DataTransformationConfig()
